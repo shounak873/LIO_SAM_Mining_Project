d4636bd62bcbd38293a2ec5931cfdce429b04996
shounak873 <shounakaero@gmail.com>
2022-07-24 18:43:07 -0400

    timing liogps

--- LIO-SAM/src/mapOptmization.cpp
+++ LIO-SAM/src/mapOptmization.cpp
@@ -33,6 +33,7 @@ using symbol_shorthand::G; // GPS pose
 #include <algorithm>
 #include <cstdlib>
 using namespace std;
+using namespace std::chrono;
 /*
     * A point cloud type that has 6D pose info ([x,y,z,roll,pitch,yaw] intensity is time stamp)
     */
@@ -168,6 +169,9 @@ public:
     std::vector<float> resvec;
     bool sparseFrame;
 
+    float timing;
+    int iter;
+
 
     // std::vector<float> c{1.0};
 
@@ -276,7 +280,8 @@ public:
         pcl::fromROSMsg(msgIn->cloud_corner,  *laserCloudCornerLast);
         pcl::fromROSMsg(msgIn->cloud_surface, *laserCloudSurfLast);
 
-
+        iter = 0;
+        timing = 0.0;
 
         std::lock_guard<std::mutex> lock(mtx);
 
@@ -1360,6 +1365,7 @@ public:
 
     void scan2MapOptimization()
     {
+        auto start = high_resolution_clock::now();
         // std::cout << " scan2MapOptimization function started " << std::endl;
         if (cloudKeyPoses3D->points.empty()){
             // std::cout << "Empty key pose vector in scan2MapOptimization " <<std::endl;
@@ -1387,6 +1393,7 @@ public:
 
                     if (LMOptimization(iterCount) == true){
                         std::cout << " converged with itercount .. "  << iterCount << std::endl;
+                        iter  = iterCount;
                         break;
                     }
                 }
@@ -1397,6 +1404,12 @@ public:
         else {
             ROS_WARN("Not enough features! Only %d edge and %d planar features available.", laserCloudCornerLastDSNum, laserCloudSurfLastDSNum);
         }
+
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+
+        timing = duration.count();
+
     }
 
     void transformUpdate()
@@ -1737,6 +1750,12 @@ public:
         laserOdometryROS.pose.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(transformTobeMapped[0], transformTobeMapped[1], transformTobeMapped[2]);
         pubLaserOdometryGlobal.publish(laserOdometryROS);
 
+        // publish dist_info
+
+        dist_info.header.stamp = timeLaserInfoStamp;
+        dist_info.iter = iter;
+        dist_info.timing = timing;
+        pubDistInfo.publish(dist_info);
 
         // Publish TF
         static tf::TransformBroadcaster br;

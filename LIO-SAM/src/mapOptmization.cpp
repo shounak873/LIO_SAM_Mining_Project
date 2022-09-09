bd0a160c6a9b67d882dcd626cf785f4b365ab34f
shounak873 <shounakaero@gmail.com>
2022-09-09 14:50:20 -0400

    added iter for non convergence

--- LIO-SAM/src/mapOptmization.cpp
+++ LIO-SAM/src/mapOptmization.cpp
@@ -1426,7 +1426,7 @@ public:
         // std::fill(resvecCorner.begin(), resvecCorner.end(), 0.0);
         // std::fill(resvecSurf.begin(), resvecSurf.end(), 0.0);
         resvec.clear();
-
+        bool converged = false;
         minalphaind = 0;
         mincind = 0;
 
@@ -1455,11 +1455,15 @@ public:
 
                     if (LMOptimization(iterCount, alpha[minalphaind], c[mincind]) == true){
                         std::cout << " converged with itercount .. "  << iterCount << std::endl;
+                        converged = true;
                         iter = iterCount;
                         break;
                     }
                 }
 
+            if(converged == false){
+                iter = 30;
+            }
             std::cout << "Best alpha value : " << bestalpha << std::endl;
             std::cout << "Best c value : " << bestc << std::endl;
 

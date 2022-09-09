09171d56e81261a070b1af41e9bfed3fa6b0dbc1
shounak873 <shounakaero@gmail.com>
2022-09-09 14:31:25 -0400

    resvec reserve added

--- LIO-SAM/src/mapOptmization.cpp
+++ LIO-SAM/src/mapOptmization.cpp
@@ -182,7 +182,7 @@ public:
         parameters.relinearizeThreshold = 0.1;
         parameters.relinearizeSkip = 1;
         isam = new ISAM2(parameters);
-
+        resvec.reserve(4000);
         // Read normalising constants from the text file;
 
         pubKeyPoses                 = nh.advertise<sensor_msgs::PointCloud2>("lio_sam/mapping/trajectory", 1);
@@ -1236,8 +1236,8 @@ public:
         std::fill(laserCloudOriCornerFlag.begin(), laserCloudOriCornerFlag.end(), false);
         std::fill(laserCloudOriSurfFlag.begin(), laserCloudOriSurfFlag.end(), false);
 
-        std::fill(resvecCorner.begin(), resvecCorner.end(), 0.0);
-        std::fill(resvecSurf.begin(), resvecSurf.end(), 0.0);
+        // std::fill(resvecCorner.begin(), resvecCorner.end(), 0.0);
+        // std::fill(resvecSurf.begin(), resvecSurf.end(), 0.0);
 
     }
 
@@ -1385,8 +1385,8 @@ public:
             return;
         }
 
-        std::fill(resvecCorner.begin(), resvecCorner.end(), 0.0);
-        std::fill(resvecSurf.begin(), resvecSurf.end(), 0.0);
+        // std::fill(resvecCorner.begin(), resvecCorner.end(), 0.0);
+        // std::fill(resvecSurf.begin(), resvecSurf.end(), 0.0);
         resvec.clear();
 
 
@@ -1395,7 +1395,7 @@ public:
             kdtreeCornerFromMap->setInputCloud(laserCloudCornerFromMapDS);
             kdtreeSurfFromMap->setInputCloud(laserCloudSurfFromMapDS);
             // std::cout << " optimization loop started .. " << std::endl;
-                for (int iterCount = 0; iterCount < 60; iterCount++)
+                for (int iterCount = 0; iterCount < 30; iterCount++)
                 {
                     laserCloudOri->clear();
                     coeffSel->clear();
